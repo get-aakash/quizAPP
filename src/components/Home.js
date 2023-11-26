c3fa53112cb7de657d523532ff80b383411ae77b
Aakash Acharya <mail.aakash108@gmail.com>
2023-11-26 12:13:57 +1100

    working on UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import  "../design/home.css";
 import questions from './Questions'
 const Home = () => {
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [quizFinished, setQuizFinished] = useState(false)
+    const [score, setScore] = useState(0)
+
+    const handleButtonClick = (value)=>{
+        value === true ? (setScore(score+1)):(console.log("error"))
+        currentIndex === questions.length-1 ? setQuizFinished(true): console.log("error")
+        setCurrentIndex(currentIndex+1)
+
+    }
     
   return (
-    <div className='container'>
-        <div className="question-section">
+    <div className='app'>
+        {quizFinished?(
+            <div className="score-section">
+                You scored {score} out of {questions.length}
+            </div>
+        ):(
+            <>
+            <div className="question-section">
             <div className="question-count">
-                <span>Question1</span>
+                <span>Question1</span>/{questions.length}
             </div>
             <div className="question-text">
-                {questions[0].questionText}
+                {questions[currentIndex].questionText}
             </div>
         </div>
-        <div className="answer-section">
-            <button>Answer 1</button>
-            <button>Answer 2</button>
-            <button>Answer 3</button>
-            <button>Answer 4</button>
+        
+            <div className="answer-section">
+           {questions[currentIndex].answerOptions.map((answer,index)=>{
+            return (
+                <button onClick={()=>handleButtonClick(answer.isCorrect)} key={index}>{answer.answerText}</button>
+            )
+           })}
         </div>
-      
+            </>
+        )}
+        
     </div>
   )
 }
